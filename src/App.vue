d702917133522f187b7263a9ccc5459e7bfcd294
hevils <hevils.crow@mail.ru>
2024-05-31 17:45:27 +0300

    added responsive web design
    - created project
    - added assets
    - created components: Filter, Showcase

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <Filter></Filter>
+  <Showcase></Showcase>
+</template>
+
+<script setup>
+import Filter from '@/components/Filter.vue'
+import Showcase from '@/components/Showcase.vue'
+
+</script>
+
+<style>
+*,
+*::before,
+*::after {
+  padding: 0;
+  margin: 0;
+  border: 0;
+  box-sizing: border-box;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+html, body {
+  font: 18px / 1.4 -apple-system, 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica', 'Arial', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+  font-weight: 500;
+}
+</style>
---
+++ src/components/Showcase.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="showcase_Wrapper">
+    <div class="showcase">
+
+    </div>
+  </div>
+</template>
+
+<script setup>
+import CharacterCard from '@/components/CharacterCard.vue'
+
+defineProps({
+  characters: Array,
+})
+
+</script>
+
+<style>
+@media (max-width: 40.625em) {
+  .showcase_Wrapper {
+    padding: 1.5rem;
+  }
+}
+.showcase_Wrapper {
+  display: flex;
+  -webkit-box-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  align-items: center;
+  padding: 4.5rem 0px;
+  background: rgb(39, 43, 51);
+  min-height: calc(-60px + 50vh);
+}
+@media (max-width: 40.625em) {
+  .showcase_Wrapper {
+    padding: 1.5rem;
+  }
+}
+.showcase {
+  display: flex;
+  -webkit-box-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  align-items: center;
+  flex-wrap: wrap;
+  max-width: 1920px;
+}
+</style>
